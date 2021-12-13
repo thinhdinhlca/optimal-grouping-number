ef07f8425d49cbf09176e52966054cffa5f10968
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2021-12-13 17:48:26 +0700

    Update function.js

--- function.js
+++ function.js
@@ -4,7 +4,7 @@ window.function = function (spg, total) {
  spg = spg.value ?? "";
  total = total.value ?? "";
   
- let threshold = spg/2;
+ const threshold = spg/2;
   
  for (var i = spg; i > 0; i--) {
    var leftovers = Math.ceil(total/spg)*spg-total;
